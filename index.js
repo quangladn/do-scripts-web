7cf50517e3257c36f858d024a6a196f59f7876a6
quangladn <laquang468135@gmail.com>
2022-07-30 18:22:45 +0700

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'); 
+require('dotenv').config()
+const app = express(); 
+const port = process.env.port || 8080; 
+const path = require('path')
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get('/images', function(req, res){
+    res.sendFile(path.join(__dirname, '/20220730_174725_0000.png'));
+})
+
+app.listen(port, function(){
+    console.log("Your app running on port " + port);
+})
+
