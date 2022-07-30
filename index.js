be443942d69ad9217321e47855586bbbb4fc1041
quangladn <laquang468135@gmail.com>
2022-07-30 18:28:27 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 require('dotenv').config()
 const app = express(); 
-const port = process.env.port || 8080; 
+const port = process.env.PORT || 8080; 
 const path = require('path')
 
 app.get('/', function(req, res){
